745356e5e9f9926039ed5aa273227679ebad9756
ouhare <44320776+ouharetaso@users.noreply.github.com>
2026-05-05 15:56:28 +0900

    fix example program

--- crates/monye/src/main.rs
+++ crates/monye/src/main.rs
@@ -89,7 +89,7 @@ fn main() -> u32 {
     ";
 
 let program = "
-fn emit_num(n: u32) -> u32 {
+fn emit_num(n: u32) {
     if n >= 100 {
         putc(48 + n / 100);
         putc(48 + n / 10 % 10);
@@ -97,14 +97,14 @@ fn emit_num(n: u32) -> u32 {
     }
     else if n >= 10 {
         putc(48 + n / 10);
-        putc(48 + n % 10)
+        putc(48 + n % 10);
     }
     else {
-        putc(48 + n)
+        putc(48 + n);
     }
 }
 
-fn emit_header() -> u32 {
+fn emit_header() {
     putc(80);
     putc(51);
     putc(10);
@@ -115,16 +115,16 @@ fn emit_header() -> u32 {
     putc(10);
 
     emit_num(255);
-    putc(10)
+    putc(10);
 }
 
-fn emit_rgb(r: u32, g: u32, b: u32) -> u32 {
+fn emit_rgb(r: u32, g: u32, b: u32) {
     emit_num(r);
     putc(32);
     emit_num(g);
     putc(32);
     emit_num(b);
-    putc(10)
+    putc(10);
 }
 
 fn lerp(a: u32, b: u32, t: u32, d: u32) -> u32 {
@@ -140,17 +140,17 @@ fn emit_lerp_rgb(
     r0: u32, g0: u32, b0: u32,
     r1: u32, g1: u32, b1: u32,
     t: u32, d: u32
-) -> u32 {
+) {
     let r: u32 = lerp(r0, r1, t, d);
     let g: u32 = lerp(g0, g1, t, d);
     let b: u32 = lerp(b0, b1, t, d);
 
-    emit_rgb(r, g, b)
+    emit_rgb(r, g, b);
 }
 
-fn emit_color(i: u32) -> u32 {
+fn emit_color(i: u32) {
     if i >= 256 {
-        emit_rgb(0, 0, 0)
+        emit_rgb(0, 0, 0);
     }
     else {
         let t: u32 = i * 255 / 256;
@@ -161,7 +161,7 @@ fn emit_color(i: u32) -> u32 {
                 0, 40, 120,
                 t,
                 31
-            )
+            );
         }
         else if t < 64 {
             emit_lerp_rgb(
@@ -169,7 +169,7 @@ fn emit_color(i: u32) -> u32 {
                 0, 180, 255,
                 t - 32,
                 31
-            )
+            );
         }
         else if t < 128 {
             emit_lerp_rgb(
@@ -177,7 +177,7 @@ fn emit_color(i: u32) -> u32 {
                 255, 255, 255,
                 t - 64,
                 63
-            )
+            );
         }
         else if t < 192 {
             emit_lerp_rgb(
@@ -185,7 +185,7 @@ fn emit_color(i: u32) -> u32 {
                 255, 180, 40,
                 t - 128,
                 63
-            )
+            );
         }
         else {
             emit_lerp_rgb(
@@ -193,7 +193,7 @@ fn emit_color(i: u32) -> u32 {
                 80, 0, 0,
                 t - 192,
                 63
-            )
+            );
         }
     }
 }
@@ -215,28 +215,28 @@ fn mandel_iter(zr: i32, zi: i32, cr: i32, ci: i32, i: u32) -> u32 {
     }
 }
 
-fn draw_point(x: i32, y: i32) -> u32 {
+fn draw_point(x: i32, y: i32) {
     let cr: i32 = -2048 + x * 4096 / 511;
     let ci: i32 = 2048 - y * 4096 / 511;
 
     let n: u32 = mandel_iter(0, 0, cr, ci, 0);
 
-    emit_color(n)
+    emit_color(n);
 }
 
-fn draw_cols(x: i32, y: i32) -> u32 {
+fn draw_cols(x: i32, y: i32) {
     if x >= 512 {
-        0
+        unit
     }
     else {
         draw_point(x, y);
-        draw_cols(x + 1, y)
+        draw_cols(x + 1, y);
     }
 }
 
-fn draw_rows(y: i32) -> u32 {
+fn draw_rows(y: i32) {
     if y >= 512 {
-        0
+        unit
     }
     else {
         draw_cols(0, y);
@@ -244,15 +244,15 @@ fn draw_rows(y: i32) -> u32 {
     }
 }
 
-fn main() -> u32 {
+fn main() {
     emit_header();
-    draw_rows(0)
+    draw_rows(0);
 }";
 
     let mut lexer = StringLexer::new(program);
     let mut tokens = lexer.lex()?;
 
-    // println!("{:?}", tokens);
+    //println!("{:?}", tokens);
 
     let ast = match parse(&mut tokens) {
         Ok(ast) => ast,
